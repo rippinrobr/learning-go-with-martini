ab41d97079a166e711ea6b99516a68d9d53b734e
Rob Rowe <rrowe@foodlogiq.com>
2014-07-29 16:48:11 -0400

    html displaying has been wired up.

--- attr-server.go
+++ attr-server.go
@@ -13,11 +13,16 @@ import (
 func main() {
   m := martini.Classic()
   m.Use( Mongo() )
-  // add middle ware here for rendering the html
-  m.Use( render.Renderer() )
-
+   m.Use( render.Renderer( render.Options{ Layout: "bland"}) )
+    
+  // API Calls
   m.Get("/attributes/:resource",  getAttributes )
   m.Post("/attributes/:resource", binding.Json( attribute{} ), addAttribute  )
+
+  // HTML Calls
+  m.Get("/", func( r render.Render) {
+	  r.HTML(200, "resources", "Rob")
+  })
 
   service := utils.ServiceDescription{"attributes", "http://localhost:3000", "Service that manages the attributes available for each resource type"}
   fmt.Println( config.RegisterService( service ) )
